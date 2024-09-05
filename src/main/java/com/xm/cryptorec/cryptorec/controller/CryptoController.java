e26626015eab4423bc0c2eef32aa680c153897e4
pkkr <p.kakarantzas@hypertech.gr>
2024-09-05 13:17:35 +0300

    added a swagger openapi3 and implemented an interface to describe my controller

--- src/main/java/com/xm/cryptorec/cryptorec/controller/CryptoController.java
+++ src/main/java/com/xm/cryptorec/cryptorec/controller/CryptoController.java
@@ -1,9 +1,12 @@
 package com.xm.cryptorec.cryptorec.controller;
 
 import com.xm.cryptorec.cryptorec.dto.CryptoNormalizedRangeDto;
+import com.xm.cryptorec.cryptorec.interfaces.CryptoControllerInterface;
 import com.xm.cryptorec.cryptorec.model.CryptoData;
 import com.xm.cryptorec.cryptorec.model.CryptoStats;
 import com.xm.cryptorec.cryptorec.service.CryptoComputationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cryptos")
-public class CryptoController {
+public class CryptoController implements CryptoControllerInterface {
 
     private final CryptoComputationService computationService;
 
---
+++ src/main/java/com/xm/cryptorec/cryptorec/interfaces/CryptoControllerInterface.java
@@ -0,0 +1,24 @@
+package com.xm.cryptorec.cryptorec.interfaces;
+
+import com.xm.cryptorec.cryptorec.dto.CryptoNormalizedRangeDto;
+import com.xm.cryptorec.cryptorec.model.CryptoStats;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@Tag(name = "Crypto Recommendation API", description = "Research your options on our system's available crypto recommendations.")
+public interface CryptoControllerInterface {
+
+    @Operation(summary = "Retrieve all available cryptos by normalized range")
+    List<CryptoNormalizedRangeDto> getCryptosSortedByNormalizedRange();
+
+    @Operation(summary = "Retrieve available crypto stats by using crypto symbol as input")
+    CryptoStats getCryptoStats(@PathVariable String symbol) throws Exception;
+
+    @Operation(summary = "Retrieve the highest normalized range for a given date")
+    @Parameter(name = "date", description = "The date for which you want to retrieve the highest normalized range", example = "2022-01-01")
+    CryptoNormalizedRangeDto getCryptoWithHighestNormalizedRange(@PathVariable String date) throws Exception;
+}
