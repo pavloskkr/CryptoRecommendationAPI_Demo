51c984e5ffc34cad15e57df2ddf964c7df143481
pkkr <p.kakarantzas@hypertech.gr>
2024-09-06 14:35:01 +0300

    fixes regarding saving csv files in cluster and implementation to read from one of two sources

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:17-jdk-alpine
 WORKDIR /app
 COPY target/cryptorec.jar cryptorec.jar
+COPY src/main/resources/data /app/data
 EXPOSE 8091
 ENTRYPOINT ["java", "-jar", "cryptorec.jar"]
--- src/main/java/com/xm/cryptorec/cryptorec/service/CsvHandler.java
+++ src/main/java/com/xm/cryptorec/cryptorec/service/CsvHandler.java
@@ -11,21 +11,28 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CsvHandler {
 
-    private static final String CSV_DIR = "src/main/resources/data/";
+    private static final String LOCAL_CSV_DIR = "src/main/resources/data/";
+    private static final String KUBERNETES_CSV_DIR = "/app/data/";
 
     public List<CryptoData> parseCsvFiles() throws IOException {
         List<CryptoData> allCryptoData = new ArrayList<>();
 
         //Dynamic scan of the /data folder
-        File folder = new File(CSV_DIR);
+        File folder = new File(KUBERNETES_CSV_DIR);
+        if (!folder.exists() || folder.listFiles() == null || Objects.requireNonNull(folder.listFiles()).length == 0) {
+            folder = new File(LOCAL_CSV_DIR);
+        }
+
         File[] csvFiles = folder.listFiles(((dir, name) -> name.toLowerCase().endsWith(".csv")));
 
         if (csvFiles == null || csvFiles.length == 0) {
-            throw new IOException("There are no .csv files in directory : " + CSV_DIR);
+            throw new IOException("There are no .csv files in either the local directory : "
+                    + LOCAL_CSV_DIR + " or the kubernetes directory : " + KUBERNETES_CSV_DIR);
         }
 
         // .csv iteration
